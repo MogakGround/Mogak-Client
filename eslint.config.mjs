2d621395db5ccd712132677186fa1350806e4001
sieun <limsieun0313@gmail.com>
2025-01-04 18:25:28 +0900

    ⚙️ Chore: 의존성 설치, 폴더 구조 및 개발 환경 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,12 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+// 플러그인과 파서 가져오기
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import tsEslintParser from '@typescript-eslint/parser';
+import globals from 'globals';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +15,41 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['node_modules/', '.next/', 'build/', 'dist/'],
 
-export default eslintConfig;
+    languageOptions: {
+      parser: tsEslintParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        sourceType: 'module',
+        createDefaultProgram: true,
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      prettier: eslintPluginPrettier,
+      '@typescript-eslint': typescriptEslintPlugin,
+    },
+    rules: {
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-filename-extension': ['warn', { extensions: ['.ts', '.tsx'] }],
+      'no-useless-catch': 'off',
+    },
+  },
+  ...compat.extends(
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier'
+  ),
+];
