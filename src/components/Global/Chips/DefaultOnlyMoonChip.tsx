8b2025370682f4831f25f66449b3053b868f97cc
inaemon <github32377@gmail.com>
2025-01-17 04:20:33 +0900

    (#2) 💄 Design: 기본 달 아이콘 칩 생성

---
+++ src/components/Global/Chips/DefaultOnlyMoonChip.tsx
@@ -0,0 +1,21 @@
+import { Color } from "@/constants/Color"
+import { getBackground, getMoonIcon } from "./DefaultChip";
+
+
+interface Params {
+    text: string;
+    size: number;
+    color: Color;
+}
+
+
+const Chip = ({size, color}: Params) => {
+    return (
+        <div className={`inline-flex items-center justify-center text-center p-[${size===30?5: size===38?8: 10}px] rounded-[4px] ${getBackground(color)} h-${size}`}>
+            {getMoonIcon(size)}
+        </div>
+    );
+}
+
+export default Chip;
+  
