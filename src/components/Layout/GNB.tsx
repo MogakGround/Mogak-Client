5c10aaae8ec536a0a344fe021e5bc7c89491b075
inaemon <github32377@gmail.com>
2025-01-19 14:11:21 +0900

    (#2) 💄 Design: GNB 헤더바 생성

---
+++ src/components/Layout/GNB.tsx
@@ -0,0 +1,45 @@
+"use client"
+import React, { useState } from 'react';
+import Image from 'next/image';
+import LogoIcon from "@/assets/svg/logo.svg"
+
+const Header = () => {
+    const [pageNumber, setPageNumber] = useState<number>(1);
+
+    const handleLogoClick = () => {
+
+    }
+    const handleHomeClick = () => {
+        setPageNumber(1);
+    }
+    const handleRankClick = () => {
+        setPageNumber(2);
+    }
+    const handleMogakClick = () => {
+        setPageNumber(3);        
+    }
+
+    return (
+        <div className="flex justify-between items-center text-center bg-bg h-[60px]">
+            <Image src={LogoIcon} alt="로고" onClick={handleLogoClick} className="ml-[80px] cursor-pointer" width={48} height={24}/>
+            
+            <div className='flex'>
+                <div className={`pr-[52px] text-16 font-semibold cursor-pointer ${pageNumber===1? "text-white": "text-grayscale-500"}`} onClick={handleHomeClick}>
+                    홈
+                </div>
+                <div className={`pr-[52px] text-16 font-semibold cursor-pointer ${pageNumber===2? "text-white": "text-grayscale-500"}`} onClick={handleRankClick}>
+                    작업자 랭킹
+                </div>
+                <div className={`text-16 font-semibold cursor-pointer ${pageNumber===3? "text-white": "text-grayscale-500"}`} onClick={handleMogakClick}>
+                    모각그라운드란
+                </div>
+            </div>
+            
+            <div className="mr-[80px] rounded-[40px] bg-grayscale-50 text-grayscale-700 text-[14px] font-semibold px-[14px] py-[7px] cursor-pointer">
+                로그인
+            </div>
+        </div>
+    );
+}
+
+export default Header;
