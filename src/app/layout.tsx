0b6cc0672bf668d3fba6c91251335a3150923e4b
sieun <limsieun0313@gmail.com>
2025-02-16 14:14:25 +0900

    (#12) ⚙️ Chore: riaSans 폰트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,27 +6,19 @@ import GNB from '@/components/layout/GNB'
 
 const pretendard = localFont({
   src: [
-    {
-      path: '../../public/fonts/Pretendard-Regular.woff',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../../public/fonts/Pretendard-Medium.woff',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../../public/fonts/Pretendard-SemiBold.woff',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: '../../public/fonts/Pretendard-Bold.woff',
-      weight: '700',
-      style: 'normal',
-    },
+    { path: '../../public/fonts/Pretendard-Regular.woff', weight: '400', style: 'normal' },
+    { path: '../../public/fonts/Pretendard-Medium.woff', weight: '500', style: 'normal' },
+    { path: '../../public/fonts/Pretendard-SemiBold.woff', weight: '600', style: 'normal' },
+    { path: '../../public/fonts/Pretendard-Bold.woff', weight: '700', style: 'normal' },
   ],
+  variable: '--font-pretendard',
+})
+
+const riaSans = localFont({
+  src: '../../public/fonts/RiaSans-ExtraBold.ttf',
+  weight: '800',
+  style: 'normal',
+  variable: '--font-riasans',
 })
 
 export const metadata: Metadata = {
@@ -40,7 +32,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={`${pretendard.variable} ${riaSans.variable}`}>
       <body className={`${pretendard.className} flex flex-col min-h-screen bg-bg`}>
         <div id="portalModal" />
         <GNB />
