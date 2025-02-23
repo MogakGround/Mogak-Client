d3a38d233429fcdc819f4ebcbb97659be8b30a32
sieun <limsieun0313@gmail.com>
2025-02-23 16:22:00 +0900

    (#29) 🔗 API: 회원가입 및 로그인 요청 파일 추가

---
+++ src/app/api/auth/auth.types.ts
@@ -0,0 +1,26 @@
+export interface PostAuthLoginRequest {
+  code: string
+}
+
+interface PostAuthLoginResponseData {
+  userId: number
+  isNewUser: boolean
+  accessToken: string
+  refreshToken: string
+}
+export interface PostAuthLoginResponse {
+  code: number
+  message: string
+  data: PostAuthLoginResponseData
+}
+
+export interface PostAuthSignUpRequest {
+  kakaoCode: string
+  nickName: string
+  portfolioUrl: string
+}
+export interface PostAuthSignUpResponse {
+  userId: number
+  accessToken: string
+  refreshToken: string
+}
