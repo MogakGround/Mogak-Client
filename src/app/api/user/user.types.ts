14dea2a820c18fe268a2c8e9db8e6c14bbb60864
sieun <limsieun0313@gmail.com>
2025-03-01 19:07:04 +0900

    (#29) 🔗 API: 유저 프로필 조회 API 추가

--- src/app/api/user/user.types.ts
+++ src/app/api/user/user.types.ts
@@ -5,3 +5,17 @@ export interface getCheckNicknameResponse {
   code: number
   message: string
 }
+
+interface getMyProfileResponseData {
+  nickName: string
+  portfolioUrl: string
+  rank: number
+  hour: number
+  min: number
+  sec: number
+}
+export interface getMyProfileResponse {
+  code: number
+  message: string
+  data: getMyProfileResponseData
+}
