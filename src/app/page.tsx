cee0ea4252139646645f7a72a186b260421a9256
inaemon <github32377@gmail.com>
2025-03-28 21:11:02 +0900

    (#35) 🪄 Feat: 홈페이지 이벤트 전체 구현 시간대 토글 이벤트 API 연동 이벤트 방 존재 여부 이벤트 페이지네이션 업뎃 이벤트 모각방 페이지 이동 기능

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ import NoMogakRoom from '@/components/app/home/NoMogakRoom'
 import { getAllRoomList, getRecentRoomList } from './api/home/api'
 import { WorkHours } from './api/room/room.types'
 import { Room } from './api/home/home.types'
+import Link from 'next/link'
 
 export default function Home() {
   // 모각방 페이지
@@ -31,22 +32,42 @@ export default function Home() {
 
   // 모각방 시간대 버튼
   const [toggleTimes, setToggleTimes] = useState<Record<string, { active: boolean; text1: string; text2: string }>>({
-    time1: { active: false, text1: '전체', text2: '' },
-    time2: { active: false, text1: '야간', text2: '오후 10시 ~ 오전 5시' },
-    time3: { active: false, text1: '오전 시간대', text2: '아침 6시 ~ 정오' },
-    time4: { active: false, text1: '오후 시간대', text2: '정오 ~ 오후 6시' },
-    time5: { active: false, text1: '저녁 시간대', text2: '오후 6시 ~ 오후 10시' },
+    time1: { active: true, text1: '전체', text2: '' },
+    time2: { active: true, text1: '야간', text2: '오후 10시 ~ 오전 5시' },
+    time3: { active: true, text1: '오전 시간대', text2: '아침 6시 ~ 정오' },
+    time4: { active: true, text1: '오후 시간대', text2: '정오 ~ 오후 6시' },
+    time5: { active: true, text1: '저녁 시간대', text2: '오후 6시 ~ 오후 10시' },
   })
 
-  // 버튼 토글
+  // 시간대 버튼 토글 이벤트
   const handleToggleTime = (id: number) => {
-    setToggleTimes((prevToggleTimes) => ({
-      ...prevToggleTimes,
-      [`time${id}`]: {
-        ...prevToggleTimes[`time${id}`],
-        active: !prevToggleTimes[`time${id}`].active, // active 상태 반전
-      },
-    }))
+    setToggleTimes((prevToggleTimes) => {
+      let updatedToggleTimes = {
+        ...prevToggleTimes,
+        [`time${id}`]: {
+          ...prevToggleTimes[`time${id}`],
+          active: !prevToggleTimes[`time${id}`].active, // active 상태 반전
+        },
+      }
+
+      if (id === 1) {
+        const newActiveState = updatedToggleTimes.time1.active
+
+        // time2~5도 time1과 동일하게 설정
+        for (let i = 2; i <= 5; i++) {
+          updatedToggleTimes[`time${i}`].active = newActiveState
+        }
+      } else {
+        // time1은 time2~5가 모두 true면 true 아니면 false
+        updatedToggleTimes.time1.active =
+          updatedToggleTimes.time2.active === true &&
+          updatedToggleTimes.time3.active === true &&
+          updatedToggleTimes.time4.active === true &&
+          updatedToggleTimes.time5.active === true
+      }
+
+      return updatedToggleTimes
+    })
   }
 
   const getSelectedWorkHours = (): WorkHours[] | null => {
@@ -80,6 +101,7 @@ export default function Home() {
     return selectedWorkHours
   }
 
+  // 시간대별 모각방 조회
   const searchAllRoomList = async () => {
     try {
       const workHours = getSelectedWorkHours()
@@ -88,7 +110,9 @@ export default function Home() {
       let data
       if (workHours) data = await getAllRoomList({ page: currentPage, size: itemsPerPage, workHours: workHours })
       else data = await getAllRoomList({ page: currentPage, size: itemsPerPage })
-      console.log(data.data.rooms)
+      console.log('시간대별 모각방 조회를 성공했습니다.')
+      //console.log(data)
+      //console.log(data.data.rooms)
 
       const rooms = data.data.rooms
       setRoomsByWorkHours(rooms)
@@ -98,105 +122,55 @@ export default function Home() {
     }
   }
 
+  // 최근 모각방 조회
   const searchRecentRoomList = async () => {
     try {
       const data = await getRecentRoomList()
-      console.log(data.data.rooms)
+      console.log('최근 모각방 조회를 성공했습니다.')
+      //console.log(data)
 
-      const rooms = data.data.rooms
+      const rooms = data.data
       setRoomsByLatest(rooms)
     } catch (error) {
       console.error(error)
     }
   }
 
-  /*
-  const filterRooms = (rooms: Room[]) => {
-    return rooms.filter((room) => {
-      if (toggleTimes.time1.active) return room.sunup && room.sun && room.sundown && room.moon
-      return (
-        (toggleTimes.time2.active && room.sunup) ||
-        (toggleTimes.time3.active && room.sun) ||
-        (toggleTimes.time4.active && room.sundown) ||
-        (toggleTimes.time5.active && room.moon)
-      )
-    })
-  }
-
-  // 조건에 맞는 모각방 구하기
-  const countRooms = () => {
-    let filtered: MogakRoomProps[] = []
-
-    const mogakRooms: MogakRoomProps[] = currentRooms
-
-    // time1에 해당하는 필터
-    if (toggleTimes.time1.active) {
-      filtered = [...filtered, ...mogakRooms.filter((room) => room.sunup && room.sun && room.sundown && room.moon)]
-    }
-
-    // time2에 해당하는 필터
-    if (toggleTimes.time2.active) {
-      filtered = [...filtered, ...mogakRooms.filter((room) => room.sunup)]
-    }
-
-    // time3에 해당하는 필터
-    if (toggleTimes.time3.active) {
-      filtered = [...filtered, ...mogakRooms.filter((room) => room.sun)]
-    }
-
-    // time4에 해당하는 필터
-    if (toggleTimes.time4.active) {
-      filtered = [...filtered, ...mogakRooms.filter((room) => room.sundown)]
-    }
-
-    // time5에 해당하는 필터
-    if (toggleTimes.time5.active) {
-      filtered = [...filtered, ...mogakRooms.filter((room) => room.moon)]
-    }
-
-    const uniqueFiltered = filtered.filter(
-      (value, index, self) =>
-        index ===
-        self.findIndex(
-          (t) =>
-            t.index === value.index && // index(고유키)를 기준으로 중복 제거
-            t.title === value.title &&
-            t.description === value.description &&
-            t.sunup === value.sunup &&
-            t.sun === value.sun &&
-            t.sundown === value.sundown &&
-            t.moon === value.moon
-        )
-    )
-
-    return uniqueFiltered
-  }
-  */
-
+  // 방이 1개 이상이면 true, 0개면 false
   const isRoom = (room: Room) => {
-    if (
-      room.workHours.includes('MORNING') &&
-      room.workHours.includes('AFTERNOON') &&
-      room.workHours.includes('NIGHT') &&
-      room.workHours.includes('LATE_NIGHT')
-    ) {
-      if (toggleTimes['time1'].active) return true
+    if (toggleTimes['time1'].active) {
+      if (
+        room.workHours.includes('MORNING') &&
+        room.workHours.includes('AFTERNOON') &&
+        room.workHours.includes('NIGHT') &&
+        room.workHours.includes('LATE_NIGHT')
+      ) {
+        return true
+      }
     }
 
-    if (room.workHours.includes('MORNING')) {
-      if (toggleTimes['time2'].active) return true
+    if (toggleTimes['time2'].active) {
+      if (room.workHours.includes('LATE_NIGHT')) {
+        return true
+      }
     }
 
-    if (room.workHours.includes('AFTERNOON')) {
-      if (toggleTimes['time3'].active) return true
+    if (toggleTimes['time3'].active) {
+      if (room.workHours.includes('MORNING')) {
+        return true
+      }
     }
 
-    if (room.workHours.includes('NIGHT')) {
-      if (toggleTimes['time4'].active) return true
+    if (toggleTimes['time4'].active) {
+      if (room.workHours.includes('AFTERNOON')) {
+        return true
+      }
     }
 
-    if (room.workHours.includes('LATE_NIGHT')) {
-      if (toggleTimes['time5'].active) return true
+    if (toggleTimes['time5'].active) {
+      if (room.workHours.includes('NIGHT')) {
+        return true
+      }
     }
 
     return false
@@ -214,13 +188,26 @@ export default function Home() {
 
   useEffect(() => {
     // API
-    searchAllRoomList() // 전체 조회
+    searchAllRoomList() // 시간대별 조회
     searchRecentRoomList() // 최근 TOP4 조회
   }, [])
 
+  // 시간대별 방 업데이트
   useEffect(() => {
-    // API
-    searchAllRoomList() // 전체 조회
+    if (
+      toggleTimes.time1.active === false &&
+      toggleTimes.time2.active === false &&
+      toggleTimes.time3.active === false &&
+      toggleTimes.time4.active === false &&
+      toggleTimes.time5.active === false
+    ) {
+      // 초기화 (방 0개)
+      setRoomsByWorkHours([])
+      setCurrentPageRoomsByWorkHours([])
+    } else {
+      // API
+      searchAllRoomList() // 시간대별 조회
+    }
   }, [toggleTimes])
 
   // 페이지네이션 및 필터링을 고려한 현재 페이지의 방들을 설정
@@ -247,21 +234,23 @@ export default function Home() {
             {roomsByLatest.map(
               (room, index) =>
                 index < 4 && (
-                  <MogakRoom
-                    index={room.roomId}
-                    key={'recent room' + index + room.roomName + room.roomId}
-                    title={room.roomName}
-                    description={room.roomExplain}
-                    thumbnailImageSrc={room.roomImg}
-                    capacity={roomCapacity}
-                    headcount={room.userCnt}
-                    sunup={room.workHours.includes('MORNING')}
-                    sun={room.workHours.includes('AFTERNOON')}
-                    sundown={room.workHours.includes('NIGHT')}
-                    moon={room.workHours.includes('LATE_NIGHT')}
-                    secret={room.isLocked}
-                    onClick={() => null}
-                  />
+                  <Link href={`/room/${room.roomId}`}>
+                    <MogakRoom
+                      index={room.roomId}
+                      key={'recent room' + index + room.roomName + room.roomId}
+                      title={room.roomName}
+                      description={room.roomExplain}
+                      thumbnailImageSrc={room.roomImg}
+                      capacity={roomCapacity}
+                      headcount={room.userCnt}
+                      sunup={room.workHours.includes('MORNING')}
+                      sun={room.workHours.includes('AFTERNOON')}
+                      sundown={room.workHours.includes('NIGHT')}
+                      moon={room.workHours.includes('LATE_NIGHT')}
+                      secret={room.isLocked}
+                      onClick={() => null}
+                    />
+                  </Link>
                 )
             )}
           </div>
@@ -284,7 +273,7 @@ export default function Home() {
                       text={toggleTimes[key].text1}
                       detailText={toggleTimes[key].text2}
                       handleClick={() => handleToggleTime(index + 1)}
-                      key={key}
+                      key={'active time chip' + index}
                     />
                   ) : (
                     <TextChip
@@ -294,7 +283,7 @@ export default function Home() {
                       text={toggleTimes[key].text1}
                       detailText={toggleTimes[key].text2}
                       handleClick={() => handleToggleTime(index + 1)}
-                      key={key}
+                      key={'non-active time chip' + index}
                     />
                   )}
                 </span>
@@ -322,21 +311,23 @@ export default function Home() {
                 {currentPageRoomsByWorkHours.map(
                   (room, index) =>
                     isRoom(room) && (
-                      <MogakRoom
-                        index={room.roomId}
-                        key={'workhours room' + index + room.roomName + room.roomId}
-                        title={room.roomName}
-                        description={room.roomExplain}
-                        thumbnailImageSrc={room.roomImg}
-                        capacity={roomCapacity}
-                        headcount={room.userCnt}
-                        sunup={room.workHours.includes('MORNING')}
-                        sun={room.workHours.includes('AFTERNOON')}
-                        sundown={room.workHours.includes('NIGHT')}
-                        moon={room.workHours.includes('LATE_NIGHT')}
-                        secret={room.isLocked}
-                        onClick={() => null}
-                      />
+                      <Link href={`/room/${room.roomId}`}>
+                        <MogakRoom
+                          index={room.roomId}
+                          key={'workhours room' + index + room.roomName + room.roomId}
+                          title={room.roomName}
+                          description={room.roomExplain}
+                          thumbnailImageSrc={room.roomImg}
+                          capacity={roomCapacity}
+                          headcount={room.userCnt}
+                          sunup={room.workHours.includes('MORNING')}
+                          sun={room.workHours.includes('AFTERNOON')}
+                          sundown={room.workHours.includes('NIGHT')}
+                          moon={room.workHours.includes('LATE_NIGHT')}
+                          secret={room.isLocked}
+                          onClick={() => null}
+                        />
+                      </Link>
                     )
                 )}
               </div>
