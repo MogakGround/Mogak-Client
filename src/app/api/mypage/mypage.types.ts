0ae2e9a6e856d228408e9461024f1c2b677adca0
inaemon <github32377@gmail.com>
2025-03-29 01:51:02 +0900

    (#38) 🪄 Feat: 마이페이지 api 구현

---
+++ src/app/api/mypage/mypage.types.ts
@@ -0,0 +1,52 @@
+import { RoomImg, WorkHours } from '../room/room.types'
+
+export interface GetChechNickNameRequest {
+  nickName: string
+}
+
+export interface PostProfileRequest {
+  nickName: string
+  portfolioUrl: string
+}
+
+export interface GetProfileResponseData {
+  nickName: string
+  portfolioUrl: string
+  rank: number
+  hour: number
+  min: number
+  sec: number
+}
+
+export interface GetRankResponseData {
+  userId: number
+  nickName: string
+  rank: number
+  hour: number
+  min: number
+  sec: number
+}
+
+export interface MyRoom {
+  roomId: number
+  roomName: string
+  roomImgUrl: RoomImg
+  roomExplain?: string
+  workHours: WorkHours[]
+  isLocked: boolean
+  roomPassword?: string
+  hour: number
+  min: number
+  sec: number
+}
+
+// 모각방 리스트 조회
+export interface GetRoomsRequest {
+  page: number
+  size: number
+}
+export interface GetRoomsResponseData {
+  totalPage: number
+  currentPage: number
+  rooms: MyRoom[]
+}
